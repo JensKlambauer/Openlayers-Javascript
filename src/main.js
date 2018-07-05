d4a6c61f6665a022f7653b12f9115f4a17647b82
Jens JK. Klambauer <Jens Klambauer@GIS2.local>
2018-07-05 10:15:43 +0200

    Update, Controls eingebaut

--- src/main.js
+++ src/main.js
@@ -3,27 +3,37 @@ import 'ol-layerswitcher/src/ol-layerswitcher.css';
 import 'ol-popup/src/ol-popup.css';
 import './styles.css';
 // 'use strict'; 
-import {Map, View} from 'ol';
+import { Map, View } from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 //import XYZ from 'ol/source/xyz';
 // import Proj from 'ol/proj';
-import {addProjection, addCoordinateTransforms, transform, get, METERS_PER_UNIT} from 'ol/proj.js';
+import { addProjection, addCoordinateTransforms, transform, get, METERS_PER_UNIT } from 'ol/proj.js';
 import Group from 'ol/layer/Group';
 import LayerSwitcher from 'ol-layerswitcher';
 import Popup from 'ol-popup';
 // import Coordinate from 'ol/coordinate';
-import {toStringHDMS} from 'ol/coordinate';
+import { toStringHDMS } from 'ol/coordinate';
 import SachsenDop from './SachsenWmsDopLayer';
 import VectorLayer from 'ol/layer/Vector.js';
 import VectorSource from 'ol/source/Vector.js';
 // import Polygon from 'ol/geom/Polygon';
 // import * as ol from 'ol';
 import { fromExtent } from 'ol/geom/Polygon.js';
-import {Circle as CircleStyle, Fill, Stroke, Style} from 'ol/style.js';
+import { Circle as CircleStyle, Fill, Stroke, Style } from 'ol/style.js';
 import Feature from 'ol/Feature.js';
+import { defaults as defaultControls } from 'ol/control.js';
+import MousePosition from 'ol/control/MousePosition.js';
+import { createStringXY } from 'ol/coordinate.js';
+
+const mousePositionControl = new MousePosition({
+  coordinateFormat: createStringXY(4),
+  projection: 'EPSG:4326',
+  undefinedHTML: 'außerhalb',
+});
 
 const map = new Map({
+  controls: defaultControls({ attributionOptions: { collapsible: true } }).extend([mousePositionControl]),
   target: 'map',
   layers: [new Group({
     'title': 'Base maps',
@@ -36,10 +46,10 @@ const map = new Map({
       }), new SachsenDop()]
   })],
   view: new View({
-    center: transform([13.2856, 51.2986], "EPSG:4326", "EPSG:3857"),
+    center: transform([12.2958, 50.6231], "EPSG:4326", "EPSG:3857"),
     projection: get("EPSG:3857"),
     //resolution: 100   
-    zoom: 15,
+    zoom: 13,
   })
 });
 console.log("map res " + map.getView().getResolution());
@@ -63,19 +73,19 @@ console.log("DPI " + window.devicePixelRatio);
 // DPI berechnen => Bildschirm abhängig
 //var DOTS_PER_INCH = 25.4 / 0.28; 
 var DOTS_PER_INCH = 96 * window.devicePixelRatio; // PixelRatio == 1
-function getResolutionFromScale(scale, dpi){
-    var units = map.getView().getProjection().getUnits();
-    var mpu = METERS_PER_UNIT[units];
-    var resolution = scale/(mpu * 39.37 * dpi);
-    return resolution;
+function getResolutionFromScale(scale, dpi) {
+  var units = map.getView().getProjection().getUnits();
+  var mpu = METERS_PER_UNIT[units];
+  var resolution = scale / (mpu * 39.37 * dpi);
+  return resolution;
 }
 
-var res = getResolutionFromScale(25000, DOTS_PER_INCH);
+var res = getResolutionFromScale(50000, 150);
 console.log("resolutionFromScale " + res);
 // map.getView().setResolution(res);
 // console.log(map.getView().getResolution())
 
-function mapScale (dpi, res) {
+function mapScale(dpi, res) {
   var unit = map.getView().getProjection().getUnits();
   var resolution = res;
   var inchesPerMetre = 39.37;
@@ -87,37 +97,40 @@ console.log("Scale MAP  " + mapScale(DOTS_PER_INCH, map.getView().getResolution(
 // https://gis.stackexchange.com/questions/242424/how-to-get-map-units-to-find-current-scale-in-openlayers
 
 let newView = new View({
-     center: transform([13.2856, 51.2986], "EPSG:4326", "EPSG:3857"),
-     projection: get("EPSG:3857"),
-     resolution: res
-  });
-  console.log(newView.calculateExtent());
-  console.log("Scale View " + mapScale(DOTS_PER_INCH, res));
+  center: map.getView().getCenter(),
+  projection: get("EPSG:3857"),
+  resolution: res
+});
 
-  const ext = newView.calculateExtent();
-  // var ext = map.getView().calculateExtent(map.getSize());
-  var feature = new Feature({   
-    'geometry': fromExtent(ext)
-   });
-  const vectorLayer = new VectorLayer({
-    source: new VectorSource({
-      wrapX: false,
-      features: [feature],
-    }),
-    style: new Style({
-      stroke: new Stroke({
-        color: 'rgba(255, 0, 0, 1.0)',
-        width: 2
-      })
+console.log("Kontrolle Scale View " + mapScale(150, res));
+// TODO: Übergabe Druckbereich in Pixel
+const ext = newView.calculateExtent([300, 300]);
+console.log(ext);
+//var ext = map.getView().calculateExtent(map.getSize());
+const feature = new Feature({
+  'geometry': fromExtent(ext)
+});
+
+const vectorLayer = new VectorLayer({
+  source: new VectorSource({
+    wrapX: false,
+    features: [feature],
+  }),
+  style: new Style({
+    stroke: new Stroke({
+      color: 'rgba(255, 0, 0, 1.0)',
+      width: 2
     })
-  });
-  map.addLayer(vectorLayer);
+  })
+});
+map.addLayer(vectorLayer);
 
 
+
+// Tests
 // const app = (a, b) => {
 //   return a + b;
 // }
-
 // console.log(app(4,5));
 
 function request(url) {
