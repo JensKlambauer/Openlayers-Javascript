5728fb681f0b316fd77696e9c69f9bc8c8dd9daf
Jens JK. Klambauer <Jens Klambauer@GIS2.local>
2018-07-10 16:15:54 +0200

    Refactoring Karte

---
+++ src/Map.js
@@ -0,0 +1,130 @@
+import { Map, View } from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+//import XYZ from 'ol/source/xyz';
+// import Proj from 'ol/proj';
+import { addProjection, addCoordinateTransforms, transform, get, METERS_PER_UNIT } from 'ol/proj.js';
+import Group from 'ol/layer/Group';
+import LayerSwitcher from 'ol-layerswitcher';
+import Popup from 'ol-popup';
+// import Coordinate from 'ol/coordinate';
+import { toStringHDMS } from 'ol/coordinate';
+import SachsenDop from './SachsenWmsDopLayer';
+import VectorLayer from 'ol/layer/Vector.js';
+import VectorSource from 'ol/source/Vector.js';
+// import Polygon from 'ol/geom/Polygon';
+// import * as ol from 'ol';
+import { fromExtent } from 'ol/geom/Polygon.js';
+import { Circle as CircleStyle, Fill, Stroke, Style } from 'ol/style.js';
+import Feature from 'ol/Feature.js';
+import { defaults as defaultControls } from 'ol/control.js';
+import MousePosition from 'ol/control/MousePosition.js';
+import { createStringXY } from 'ol/coordinate.js';
+
+const mousePositionControl = new MousePosition({
+    coordinateFormat: createStringXY(4),
+    projection: 'EPSG:4326',
+    undefinedHTML: 'außerhalb',
+});
+
+const DOTS_PER_INCH = 96;
+export { DOTS_PER_INCH };
+
+export default class PrintingMap {
+    constructor() {
+        this.map = new Map({
+            controls: defaultControls({ attributionOptions: { collapsible: true } }).extend([mousePositionControl]),
+            target: 'map',
+            layers: [new Group({
+                'title': 'Base maps',
+                layers: [
+                    new TileLayer({
+                        title: 'OSM',
+                        type: 'base',
+                        visible: true,
+                        source: new OSM()
+                    }), new SachsenDop()]
+            })],
+            view: new View({
+                center: transform([12.2958, 50.6231], "EPSG:4326", "EPSG:3857"),
+                projection: get("EPSG:3857"),
+                //resolution: 100   
+                zoom: 13,
+            })
+        });
+        console.log("map res " + this.map.getView().getResolution());
+
+        var layerSwitcher = new LayerSwitcher({
+            tipLabel: 'Legende' // Optional label for button
+        });
+        this.map.addControl(layerSwitcher);
+
+        var popup = new Popup();
+        this.map.addOverlay(popup);
+
+        this.map.on('singleclick', function (evt) {
+            //alert("Test");
+            var prettyCoord = toStringHDMS(transform(evt.coordinate, 'EPSG:3857', 'EPSG:4326'), 2);
+            popup.show(evt.coordinate, '<div><h2>Coordinates</h2><p>' + prettyCoord + '</p></div>');
+        });      
+    }
+
+    getResolutionFromScale(scale, dpi) {
+        var units = this.map.getView().getProjection().getUnits();
+        var mpu = METERS_PER_UNIT[units];
+        var resolution = scale / (mpu * 39.37 * dpi);
+        return resolution;
+    }
+
+    mapScale(dpi, res) {
+        var unit = this.map.getView().getProjection().getUnits();
+        var resolution = res;
+        var inchesPerMetre = 39.37;
+
+        return resolution * METERS_PER_UNIT[unit] * inchesPerMetre * dpi;
+    }
+
+    onMoveEnd(evt) {
+        console.log("MoveEnd");
+        this.printSource.clear();
+        console.log("Center " + this.map.getView().getCenter())
+        let newView = new View({
+            center: this.map.getView().getCenter(),
+            projection: get("EPSG:3857"),
+            resolution: this.res
+        });
+
+        console.log("Kontrolle Scale View " + this.mapScale(DOTS_PER_INCH, this.res));
+        // TODO: Übergabe Druckbereich in Pixel
+        const ext = newView.calculateExtent([462, 445]);
+        console.log(ext);
+        //var ext = map.getView().calculateExtent(map.getSize());
+        const feature = new Feature({
+            'geometry': fromExtent(ext)
+        });
+        this.printSource.addFeature(feature);
+    }
+
+    addPrintLayer() {
+        this.printSource = new VectorSource({
+            wrapX: false,
+            features: [],
+        });
+
+        this.printLayer = new VectorLayer({
+            source: this.printSource,
+            style: new Style({
+                stroke: new Stroke({
+                    color: 'rgba(255, 0, 0, 1.0)',
+                    width: 2
+                })
+            })
+        });
+        this.map.addLayer(this.printLayer);
+
+        this.res = this.getResolutionFromScale(50000, DOTS_PER_INCH);
+        this.map.on('moveend', (evt) => {
+           this.onMoveEnd(evt);
+        });
+    }    
+}
--- src/main.js
+++ src/main.js
@@ -2,138 +2,167 @@ import 'ol/ol.css';
 import 'ol-layerswitcher/src/ol-layerswitcher.css';
 import 'ol-popup/src/ol-popup.css';
 import './styles.css';
-// 'use strict'; 
-import { Map, View } from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
-//import XYZ from 'ol/source/xyz';
-// import Proj from 'ol/proj';
-import { addProjection, addCoordinateTransforms, transform, get, METERS_PER_UNIT } from 'ol/proj.js';
-import Group from 'ol/layer/Group';
-import LayerSwitcher from 'ol-layerswitcher';
-import Popup from 'ol-popup';
-// import Coordinate from 'ol/coordinate';
-import { toStringHDMS } from 'ol/coordinate';
-import SachsenDop from './SachsenWmsDopLayer';
-import VectorLayer from 'ol/layer/Vector.js';
-import VectorSource from 'ol/source/Vector.js';
-// import Polygon from 'ol/geom/Polygon';
-// import * as ol from 'ol';
-import { fromExtent } from 'ol/geom/Polygon.js';
-import { Circle as CircleStyle, Fill, Stroke, Style } from 'ol/style.js';
-import Feature from 'ol/Feature.js';
-import { defaults as defaultControls } from 'ol/control.js';
-import MousePosition from 'ol/control/MousePosition.js';
-import { createStringXY } from 'ol/coordinate.js';
+import PrintingMap, { DOTS_PER_INCH } from './Map';
 
-const mousePositionControl = new MousePosition({
-  coordinateFormat: createStringXY(4),
-  projection: 'EPSG:4326',
-  undefinedHTML: 'außerhalb',
+
+ready(function () {
+  console.log("Karte ready!");
+  const map = new PrintingMap();
+  map.addPrintLayer();
+
+  console.log(DOTS_PER_INCH);
+  console.log("DPI Factor " + window.devicePixelRatio);
 });
 
-const map = new Map({
-  controls: defaultControls({ attributionOptions: { collapsible: true } }).extend([mousePositionControl]),
-  target: 'map',
-  layers: [new Group({
-    'title': 'Base maps',
-    layers: [
-      new TileLayer({
-        title: 'OSM',
-        type: 'base',
-        visible: true,
-        source: new OSM()
-      }), new SachsenDop()]
-  })],
-  view: new View({
-    center: transform([12.2958, 50.6231], "EPSG:4326", "EPSG:3857"),
-    projection: get("EPSG:3857"),
-    //resolution: 100   
-    zoom: 13,
-  })
-});
-console.log("map res " + map.getView().getResolution());
-
-var layerSwitcher = new LayerSwitcher({
-  tipLabel: 'Legende' // Optional label for button
-});
-map.addControl(layerSwitcher);
-
-var popup = new Popup();
-map.addOverlay(popup);
-
-map.on('singleclick', function (evt) {
-  //alert("Test");
-  var prettyCoord = toStringHDMS(transform(evt.coordinate, 'EPSG:3857', 'EPSG:4326'), 2);
-  popup.show(evt.coordinate, '<div><h2>Coordinates</h2><p>' + prettyCoord + '</p></div>');
-});
-
-console.log("DPI Factor " + window.devicePixelRatio);
-
-// DPI berechnen => Bildschirm abhängig
-var DOTS_PER_INCH = 96; // * window.devicePixelRatio; // PixelRatio == 1
-function getResolutionFromScale(scale, dpi) {
-  var units = map.getView().getProjection().getUnits();
-  var mpu = METERS_PER_UNIT[units];
-  var resolution = scale / (mpu * 39.37 * dpi);
-  return resolution;
-}
-// Karte Maßstab setzen
-// map.getView().setResolution(getResolutionFromScale(80000, DOTS_PER_INCH));
-
-// console.log("resolutionFromScale " + res);
-// map.getView().setResolution(res);
-// console.log(map.getView().getResolution())
-
-function mapScale(dpi, res) {
-  var unit = map.getView().getProjection().getUnits();
-  var resolution = res;
-  var inchesPerMetre = 39.37;
-
-  return resolution * METERS_PER_UNIT[unit] * inchesPerMetre * dpi;
+function ready(callback) {
+  // in case the document is already rendered
+  if (document.readyState !== "loading") {
+    callback();
+  } else if (document.addEventListener) {
+    document.addEventListener("DOMContentLoaded", callback);
+  } else {
+    document.addEventListener("onreadystatechange", function () {
+      if (document.readyState === "complete") {
+        callback();
+      }
+    });
+  }
 }
 
-console.log("Scale MAP  " + mapScale(DOTS_PER_INCH, map.getView().getResolution()));
 
-// Druckbereich, Maßstab 1 : 50.000
-var res = getResolutionFromScale(50000, DOTS_PER_INCH);
-const printSource = new VectorSource({
-  wrapX: false,
-  features: [],
-});
-const printLayer = new VectorLayer({
-  source: printSource,
-  style: new Style({
-    stroke: new Stroke({
-      color: 'rgba(255, 0, 0, 1.0)',
-      width: 2
-    })
-  })
-});
-map.addLayer(printLayer);
 
-function onMoveEnd(evt) {
-  console.log("MoveEnd");
-  printSource.clear();
-  console.log("Center " + map.getView().getCenter())
-  let newView = new View({
-    center: map.getView().getCenter(),
-    projection: get("EPSG:3857"),
-    resolution: res
-  });
-  
-  console.log("Kontrolle Scale View " + mapScale(DOTS_PER_INCH, res));
-  // TODO: Übergabe Druckbereich in Pixel
-  const ext = newView.calculateExtent([462, 445]);
-  console.log(ext);
-  //var ext = map.getView().calculateExtent(map.getSize());
-  const feature = new Feature({
-    'geometry': fromExtent(ext)
-  });
-  printSource.addFeature(feature);  
-}
+// // 'use strict'; 
+// import { Map, View } from 'ol';
+// import TileLayer from 'ol/layer/Tile';
+// import OSM from 'ol/source/OSM';
+// // import XYZ from 'ol/source/xyz';
+// // import Proj from 'ol/proj';
+// import { addProjection, addCoordinateTransforms, transform, get, METERS_PER_UNIT } from 'ol/proj.js';
+// import Group from 'ol/layer/Group';
+// import LayerSwitcher from 'ol-layerswitcher';
+// import Popup from 'ol-popup';
+// // import Coordinate from 'ol/coordinate';
+// import { toStringHDMS } from 'ol/coordinate';
+// import SachsenDop from './SachsenWmsDopLayer';
+// import VectorLayer from 'ol/layer/Vector.js';
+// import VectorSource from 'ol/source/Vector.js';
+// // import Polygon from 'ol/geom/Polygon';
+// // import * as ol from 'ol';
+// import { fromExtent } from 'ol/geom/Polygon.js';
+// import { Circle as CircleStyle, Fill, Stroke, Style } from 'ol/style.js';
+// import Feature from 'ol/Feature.js';
+// import { defaults as defaultControls } from 'ol/control.js';
+// import MousePosition from 'ol/control/MousePosition.js';
+// import { createStringXY } from 'ol/coordinate.js';
 
-map.on('moveend', onMoveEnd);
+// const mousePositionControl = new MousePosition({
+//   coordinateFormat: createStringXY(4),
+//   projection: 'EPSG:4326',
+//   undefinedHTML: 'außerhalb',
+// });
+
+// const map = new Map({
+//   controls: defaultControls({ attributionOptions: { collapsible: true } }).extend([mousePositionControl]),
+//   target: 'map',
+//   layers: [new Group({
+//     'title': 'Base maps',
+//     layers: [
+//       new TileLayer({
+//         title: 'OSM',
+//         type: 'base',
+//         visible: true,
+//         source: new OSM()
+//       }), new SachsenDop()]
+//   })],
+//   view: new View({
+//     center: transform([12.2958, 50.6231], "EPSG:4326", "EPSG:3857"),
+//     projection: get("EPSG:3857"),
+//     //resolution: 100   
+//     zoom: 13,
+//   })
+// });
+// console.log("map res " + map.getView().getResolution());
+
+// var layerSwitcher = new LayerSwitcher({
+//   tipLabel: 'Legende' // Optional label for button
+// });
+// map.addControl(layerSwitcher);
+
+// var popup = new Popup();
+// map.addOverlay(popup);
+
+// map.on('singleclick', function (evt) {
+//   //alert("Test");
+//   var prettyCoord = toStringHDMS(transform(evt.coordinate, 'EPSG:3857', 'EPSG:4326'), 2);
+//   popup.show(evt.coordinate, '<div><h2>Coordinates</h2><p>' + prettyCoord + '</p></div>');
+// });
+
+// console.log("DPI Factor " + window.devicePixelRatio);
+
+// // DPI berechnen => Bildschirm abhängig
+// var DOTS_PER_INCH = 96; // * window.devicePixelRatio; // PixelRatio == 1
+// function getResolutionFromScale(scale, dpi) {
+//   var units = map.getView().getProjection().getUnits();
+//   var mpu = METERS_PER_UNIT[units];
+//   var resolution = scale / (mpu * 39.37 * dpi);
+//   return resolution;
+// }
+// // Karte Maßstab setzen
+// // map.getView().setResolution(getResolutionFromScale(80000, DOTS_PER_INCH));
+
+// // console.log("resolutionFromScale " + res);
+// // map.getView().setResolution(res);
+// // console.log(map.getView().getResolution())
+
+// function mapScale(dpi, res) {
+//   var unit = map.getView().getProjection().getUnits();
+//   var resolution = res;
+//   var inchesPerMetre = 39.37;
+
+//   return resolution * METERS_PER_UNIT[unit] * inchesPerMetre * dpi;
+// }
+
+// console.log("Scale MAP  " + mapScale(DOTS_PER_INCH, map.getView().getResolution()));
+
+// // Druckbereich, Maßstab 1 : 50.000
+// var res = getResolutionFromScale(50000, DOTS_PER_INCH);
+// const printSource = new VectorSource({
+//   wrapX: false,
+//   features: [],
+// });
+// const printLayer = new VectorLayer({
+//   source: printSource,
+//   style: new Style({
+//     stroke: new Stroke({
+//       color: 'rgba(255, 0, 0, 1.0)',
+//       width: 2
+//     })
+//   })
+// });
+// map.addLayer(printLayer);
+
+// function onMoveEnd(evt) {
+//   console.log("MoveEnd");
+//   printSource.clear();
+//   console.log("Center " + map.getView().getCenter())
+//   let newView = new View({
+//     center: map.getView().getCenter(),
+//     projection: get("EPSG:3857"),
+//     resolution: res
+//   });
+
+//   console.log("Kontrolle Scale View " + mapScale(DOTS_PER_INCH, res));
+//   // TODO: Übergabe Druckbereich in Pixel
+//   const ext = newView.calculateExtent([462, 445]);
+//   console.log(ext);
+//   //var ext = map.getView().calculateExtent(map.getSize());
+//   const feature = new Feature({
+//     'geometry': fromExtent(ext)
+//   });
+//   printSource.addFeature(feature);  
+// }
+
+// map.on('moveend', onMoveEnd);
 
 
 
@@ -143,49 +172,49 @@ map.on('moveend', onMoveEnd);
 // }
 // console.log(app(4,5));
 
-function request(url) {
-  return new Promise(function (resolve, reject) {
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function (e) {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          resolve(xhr.response)
-        } else {
-          reject({
-            status: this.status,
-            statusText: xhr.statusText
-          });
-        }
-      }
-    }
-    xhr.ontimeout = function () {
-      reject('timeout')
-    }
-    xhr.open('get', url, true)
-    xhr.send()
-  })
-}
+// function request(url) {
+//   return new Promise(function (resolve, reject) {
+//     const xhr = new XMLHttpRequest();
+//     xhr.onreadystatechange = function (e) {
+//       if (xhr.readyState === 4) {
+//         if (xhr.status === 200) {
+//           resolve(xhr.response)
+//         } else {
+//           reject({
+//             status: this.status,
+//             statusText: xhr.statusText
+//           });
+//         }
+//       }
+//     }
+//     xhr.ontimeout = function () {
+//       reject('timeout')
+//     }
+//     xhr.open('get', url, true)
+//     xhr.send()
+//   })
+// }
 
-function handleRepoList(user, repos) {
-  const userRepos = JSON.parse(repos)
+// function handleRepoList(user, repos) {
+//   const userRepos = JSON.parse(repos)
 
-  // Handle each individual user repo here
-  console.log(user, userRepos)
-}
+//   // Handle each individual user repo here
+//   console.log(user, userRepos)
+// }
 
-async function list() {
-  const userGet = `https://api.github.com/search/users?page=1&q=jensklambauer&type=Users`;
+// async function list() {
+//   const userGet = `https://api.github.com/search/users?page=1&q=jensklambauer&type=Users`;
 
-  const users = await request(userGet);
-  console.log(users)
-  const usersList = JSON.parse(users).items;
+//   const users = await request(userGet);
+//   console.log(users)
+//   const usersList = JSON.parse(users).items;
 
-  usersList.forEach(async function (user) {
-    const repos = await request(user.repos_url)
+//   usersList.forEach(async function (user) {
+//     const repos = await request(user.repos_url)
 
-    handleRepoList(user, repos)
-  })
-}
+//     handleRepoList(user, repos)
+//   })
+// }
 
 // import WMTSCapabilities from 'ol/format/wmtscapabilities';
 // import WMSCapabilities from 'ol/format/wmscapabilities';
