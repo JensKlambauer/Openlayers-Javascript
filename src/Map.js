501a45671c82510fb779425f503be0c17e88d1ab
Jens JK. Klambauer <Jens Klambauer@GIS2.local>
2018-07-24 17:02:37 +0200

    Selection Feature

--- src/Map.js
+++ src/Map.js
@@ -21,6 +21,8 @@ import { defaults as defaultControls } from 'ol/control.js';
 import MousePosition from 'ol/control/MousePosition.js';
 import { createStringXY } from 'ol/coordinate.js';
 import { unByKey } from 'ol/Observable';
+import Select from 'ol/interaction/Select'
+import {click, pointerMove, altKeyOnly} from 'ol/events/condition.js';
 
 const mousePositionControl = new MousePosition({
     coordinateFormat: createStringXY(4),
@@ -60,16 +62,17 @@ export default class PrintingMap {
         });
         this.map.addControl(layerSwitcher);
 
-        var popup = new Popup();
-        this.map.addOverlay(popup);
+        // Popup 
+        // var popup = new Popup();
+        // this.map.addOverlay(popup);
 
-        this.map.on('singleclick', function (evt) {
-            //alert("Test");
-            var prettyCoord = toStringHDMS(transform(evt.coordinate, 'EPSG:3857', 'EPSG:4326'), 2);
-            popup.show(evt.coordinate, '<div><h2>Coordinates</h2><p>' + prettyCoord + '</p></div>');
-        });
+        // this.map.on('singleclick', function (evt) {
+        //     //alert("Test");
+        //     var prettyCoord = toStringHDMS(transform(evt.coordinate, 'EPSG:3857', 'EPSG:4326'), 2);
+        //     popup.show(evt.coordinate, '<div><h2>Coordinates</h2><p>' + prettyCoord + '</p></div>');
+        // });
 
-        this.showPrintBox = false;
+        this.showPrintBox = false;        
     }
 
     get extentsPrint() {
@@ -93,24 +96,22 @@ export default class PrintingMap {
 
     onMoveEnd(evt) {
         // console.log("MoveEnd");
-        this.printSource.clear();       
-
-        let newView = new View({
-            center: this.map.getView().getCenter(),
-            projection: get("EPSG:3857"),
-            resolution: this.res
-        });
-
-        // console.log("Kontrolle Scale View " + this.mapScale(DOTS_PER_INCH, this.res));       
-        this.ext = newView.calculateExtent([this.width, this.height]);
+        this.printSource.clear(); 
+        let center = this.map.getView().getCenter();
+        this.ext = this.getExtPrintView(center);
         console.log("Center " + this.map.getView().getCenter())
-        console.log("width: " + this.width + " height: " + this.height);
-        console.log(this.ext);
+        // console.log("width: " + this.width + " height: " + this.height);
+        // console.log(this.ext);
+
+        // Bounds Map        
         //var ext = this.map.getView().calculateExtent(this.map.getSize());
-        const feature = new Feature({
-            'geometry': fromExtent(this.ext)
-        });
-        this.printSource.addFeature(feature);
+        
+        this.addFeaturePrint(this.ext);
+
+        // TODO: Code für Raster/Gitter        
+        let ext1 = this.getExtPrintView([center[0] + (this.ext[2] - this.ext[0]), center[1]])
+        // console.log(ext1);
+        this.addFeaturePrint(ext1);
     }
 
     addPrintLayer(scale, width, height, dpi) {
@@ -144,6 +145,43 @@ export default class PrintingMap {
         });
 
         this.showPrintBox = true;
+        let select = new Select({
+            condition: click,
+            //layers: function (layer) {
+            //    return /* some logic on layer to decide if its features should be considered; return true if yes */;
+            //},
+            // filter: function(feature, layer) {
+            //     /* some logic on a feature and layer to decide if it should be selectable; return true if yes */
+            //     return layer === this.printLayer;
+            // }
+        });
+
+        //this.map.removeInteraction(select);
+        this.map.addInteraction(select);
+        // https://openlayers.org/en/latest/examples/hit-tolerance.html
+        this.map.on('singleclick', function(e) {
+            var hit = false;
+            this.map.forEachFeatureAtPixel(e.pixel, function() {
+              hit = true;
+            }, {
+              hitTolerance: hitTolerance
+            });
+            if (hit) {
+              style.getStroke().setColor('green');
+            } else {
+              style.getStroke().setColor('black');
+            }
+            feature.changed();
+          });
+        // select.on('select', function(evt) {
+        //     console.log("Klick");
+        //     this.map.forEachFeatureAtPixel(evt.pixel,
+        //         function(feature, layer) {
+        //             if (layer === this.printLayer) {                      
+        //                 console.log("printLayer");
+        //             }});
+        // });
+
         this.onMoveEnd();
         // this.map.renderSync();
     }
@@ -157,5 +195,24 @@ export default class PrintingMap {
             this.ext = null;         
         }           
         this.showPrintBox = false;
+    }
+
+    getExtPrintView(center) {
+        let newView = new View({
+            center: center,
+            projection: get("EPSG:3857"),
+            resolution: this.res
+        });
+
+        // console.log("Kontrolle Scale View " + this.mapScale(DOTS_PER_INCH, this.res));       
+        return newView.calculateExtent([this.width, this.height]);
+    }
+
+    addFeaturePrint(extents) {
+        const feature = new Feature({
+            'geometry': fromExtent(extents)
+        });
+        console.log(feature);
+        this.printSource.addFeature(feature);
     }
 }
